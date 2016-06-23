c51d58361752f7648a1ac17ba674c679e5fbda0c
gerry3309 <gerhard.mlady@hotmail.de>
2016-06-23 13:58:43 +0200

    trying to make it work on codesigns46

--- SSH Hello World/kernel.cu
+++ SSH Hello World/kernel.cu
@@ -1,8 +1,16 @@
+#include <math.h>
+#include <iostream>
 
+// Shared Library Test Functions
+//#include <helper_functions.h>  // CUDA SDK Helper functions
+  
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/gpu/gpu.hpp>
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
